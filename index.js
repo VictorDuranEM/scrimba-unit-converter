0476c95b93db1de4498cb2638d8c5a0356f32c0c
Victor Duran <victor.duran.em@gmail.com>
2022-09-01 11:16:46 -0500

    Some fixes

--- index.js
+++ index.js
@@ -5,13 +5,13 @@
 */
 
 // Elements
-unitInputEl = document.getElementById("unit-input")
-lengthOutputEl = document.getElementById("length-output")
-volumeOutputEl = document.getElementById("volume-output")
-massOutputEl = document.getElementById("mass-output")
+const unitInputEl = document.getElementById("unit-input")
+const lengthOutputEl = document.getElementById("length-output")
+const volumeOutputEl = document.getElementById("volume-output")
+const massOutputEl = document.getElementById("mass-output")
 
 // Button
-convertBtn = document.getElementById("convert-btn")
+const convertBtn = document.getElementById("convert-btn")
 
 // Conversion numbers
 const feetPerMeter = 3.28084
@@ -19,33 +19,33 @@ const gallonsPerLiter = 0.264172
 const poundsPerKilo = 2.20462
 
 // Run calculate on app start
-calculate()
+calculateAllUnits()
 
 convertBtn.addEventListener("click", function() {
-  calculate()
+  calculateAllUnits()
 })
 
-function calculate() {
-  let unitInput = Number(unitInputEl.value)
+function calculateAllUnits() {
+  const unitInput = Number(unitInputEl.value)
   calculateLength(unitInput)
   calculateVolume(unitInput)
   calculateMass(unitInput)
 }
 
 function calculateLength(unitInput) {
-  let feetFromMeters = (unitInput * feetPerMeter).toFixed(3)
-  let metersFromFeet = (unitInput / feetPerMeter).toFixed(3)
-  lengthOutputEl.textContent = `${unitInput} meters = ${feetFromMeters} feet | 20 feet = ${metersFromFeet} meters`
+  const feetFromMeters = (unitInput * feetPerMeter).toFixed(3)
+  const metersFromFeet = (unitInput / feetPerMeter).toFixed(3)
+  lengthOutputEl.textContent = `${unitInput} meters = ${feetFromMeters} feet | ${unitInput} feet = ${metersFromFeet} meters`
 }
 
 function calculateVolume(unitInput) {
-  let gallonsFromLiters = (unitInput * gallonsPerLiter).toFixed(3)
-  let litersFromGallons = (unitInput / gallonsPerLiter).toFixed(3)
-  volumeOutputEl.textContent = `${unitInput} liters = ${gallonsFromLiters} gallons | 20 gallons = ${litersFromGallons} liters`
+  const gallonsFromLiters = (unitInput * gallonsPerLiter).toFixed(3)
+  const litersFromGallons = (unitInput / gallonsPerLiter).toFixed(3)
+  volumeOutputEl.textContent = `${unitInput} liters = ${gallonsFromLiters} gallons | ${unitInput} gallons = ${litersFromGallons} liters`
 }
 
 function calculateMass(unitInput) {
-  let poundsFromKilos = (unitInput * poundsPerKilo).toFixed(3)
-  let kilosFromPounds = (unitInput / poundsPerKilo).toFixed(3)
-  massOutputEl.textContent = `${unitInput} kilos = ${poundsFromKilos} pounds | 20 pounds = ${kilosFromPounds} kilos`
+  const poundsFromKilos = (unitInput * poundsPerKilo).toFixed(3)
+  const kilosFromPounds = (unitInput / poundsPerKilo).toFixed(3)
+  massOutputEl.textContent = `${unitInput} kilos = ${poundsFromKilos} pounds | ${unitInput} pounds = ${kilosFromPounds} kilos`
 }
